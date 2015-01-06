71c8ca87c2f6cd63e30436fa6169978e549b799d
Craig Peterson <cpeterson@ancestry.com>
2015-01-06 08:49:19 -0700

    script dir env variable

--- main.go
+++ main.go
@@ -3,11 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/captncraig/github-webhooks"
+	"log"
 	"net/http"
+	"os"
+	"path"
 	"strings"
 )
 
+var scriptDir string
+
+func init() {
+	scriptDir = os.Getenv("TINYCI-SCRIPT-DIR")
+
+	if scriptDir == "" {
+		var err error
+		scriptDir, err = os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		scriptDir = path.Join(scriptDir, "scripts")
+	}
+}
+
 func main() {
+
 	gitHooks := webhooks.WebhookListener{}
 	gitHooks.OnPush = githubHook
 	http.HandleFunc("/gh", gitHooks.GetHttpListener())
@@ -18,7 +37,8 @@ func githubHook(event *webhooks.PushEvent, _ *webhooks.WebhookContext) {
 	repo := strings.Replace(event.Repository.FullName, "/", ".", -1)
 	refPath := strings.Split(event.Ref, "/")
 	ref := refPath[len(refPath)-1]
-	fmt.Printf("%s.sh\n", repo)
-	fmt.Printf("%s:%s.sh\n", repo, ref)
-
+	anyBranchScript := path.Join(scriptDir, fmt.Sprintf("%s.sh", repo))
+	fmt.Println(anyBranchScript)
+	singleBranchScript := path.Join(scriptDir, fmt.Sprintf("%s:%s.sh", repo, ref))
+	fmt.Println(singleBranchScript)
 }
